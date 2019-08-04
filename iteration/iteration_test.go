6bed0a9deee97c09efad40439e88445d1af12d48
Alexander Wahl-Rasmussen <a.wahl.r@gmail.com>
2019-08-04 23:40:01 +0200

    a structural approach

--- iteration/iteration_test.go
+++ iteration/iteration_test.go
@@ -9,7 +9,7 @@ func TestRepeat(t *testing.T) {
 	repeated := Repeat("a", 4)
 	expected := "aaaa"
 
-	if repeated != expected {
+	if (repeated != expected) {
 		t.Errorf("expected %q but got %q", expected, repeated)
 	}
 }
